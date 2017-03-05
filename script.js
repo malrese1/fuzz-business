78d9d78cdcae9b06a2f844bdade014cd027ee5f0
superbuggy <jpqreichard@gmail.com>
2017-03-05 13:44:11 -0500

    add james' solution

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function sumFizzBuzzFactors(maximum){
-  // return the sum of the numbers between 0 and maximum that are divisible by 3 or 5
+    let range = [...Array(maximum).keys()];
+    return range
+      .filter( num => (num % 3 === 0 || num % 5 === 0 ) )
+      .reduce( (a,b) => a + b );
 }
