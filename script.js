53e527bd767bd1619132ccb2f563797ace90534e
Adrian Maseda <amaseda@gmail.com>
2017-03-05 14:01:43 -0500

    add forEach soln

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Solution: .forEach
+function sumFizzBuzzFactors(maximum, arr=genArray(maximum)){
+  let sum = 0
+  arr.forEach(num => {
+    if(!(num % 3) || !(num % 5)){
+      sum += num
+    }
+  })
+  return sum
+}
+
 // Solution: .reduce
 function sumFizzBuzzFactors(maximum, arr=genArray(maximum)){
   return arr.reduce((acc, curr) => {
