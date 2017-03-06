d8903a48686979592790e8ead00920bfb995fa17
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-03-06 10:23:28 -0500

    code challenge

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function sumFizzBuzzFactors(maximum){
   // return the sum of the numbers between 0 and maximum that are divisible by 3 or 5
+  let sum = 0;
+  for (let i=o; i < maximum; i++) {
+    sum += (i % 3 === 0 || i % 5 === 0) ? i : ;
+  }
+  return sum;
+}
+
+
+function genArray (maximum){
+let range = []
+for (var i = 0; i < maximum; i++) {
+  range.push(i)
+}
+return range
 }
